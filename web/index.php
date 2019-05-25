4bda550e2f50cfa76dadb6986d200460d339f864
danilo patrucco <dp676@njit.edu>
2019-05-25 00:09:46 -0400

    added database access

--- web/index.php
+++ web/index.php
@@ -36,4 +36,37 @@ $app->get('/', function() use($app) {
   return str_repeat('Hello', getenv('TIMES'));
 });
 
+//DATABASE_URL PDO connection
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+
+//Query database_url handlers
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
 $app->run();
